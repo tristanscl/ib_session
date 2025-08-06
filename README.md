c4c9fc94fd77d40222d1651d7b5f432d5989b7cf
tristanscl <tristanscuillerjacq@gmail.com>
2025-08-06 16:28:15 +0200

    fixed name inconstitencies

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# `ib_session`
+# `ib_utils`
 
 Simple context manager for Interactive Brokers connections using `ib_async`.
 
 ## Installation
 ```bash
-pip install git+https://github.com/tristanscl/ib_session
+pip install git+https://github.com/tristanscl/ib_utils
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ib_utils",
-    version="0.1.0",
+    version="0.1.1",
     description="Context manager for Interactive Brokers sessions with ib_async",
     author="Tristan Scuiller",
     packages=find_packages(where="src"),
